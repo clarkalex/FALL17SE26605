d91343e22c205ecd5b9b0026b8560ffbc4766de2
Clark Alexander <calexander@neit.edu>
2017-10-16 08:20:46 -0400

    Basic template

---
+++ wk3demo/assets/header.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: calexande
+ * Date: 10/16/2017
+ * Time: 8:12 AM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <!-- css links goes here -->
+    <!-- javascript goes here -->
+</head>
+<body>
+<header>
+    <nav></nav>
+</header>
+<section>
