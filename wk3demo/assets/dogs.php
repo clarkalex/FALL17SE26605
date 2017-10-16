8098b2b3fdb0e9e121ffafbf7682dcb3982c144c
Clark Alexander <calexander@neit.edu>
2017-10-16 10:27:27 -0400

    Add dog

--- wk3demo/assets/dogs.php
+++ wk3demo/assets/dogs.php
@@ -11,13 +11,29 @@ function getDogsAsTable($db) {
         $sql = $db->prepare($sql);
         $sql->execute();
         $dogs = $sql->fetchAll(PDO::FETCH_ASSOC);
-        $table = "<table>" . PHP_EOL;
-        foreach ($dogs as $dog) {
-            $table .= "<tr><td>" . $dog['name'] . "</td><td>" . $dog['gender'] . "</td><td>" . $dog['fixed'] . "</td></tr>";
+        if ($sql->rowCount() > 0) {
+            $table = "<table>" . PHP_EOL;
+            foreach ($dogs as $dog) {
+                $table .= "<tr><td>" . $dog['name'] . "</td><td>" . $dog['gender'] . "</td><td>" . $dog['fixed'] . "</td></tr>";
+            }
+            $table .= "</table>" . PHP_EOL;
+        } else {
+            $table = "Life is sad. There are no dogs.". PHP_EOL;
         }
-        $table .= "</table>" . PHP_EOL;
         return $table;
     } catch (PDOException $e){
         die("There was a problem getting the dogs");
+    }
+}
+function addDog($db, $name, $gender, $fixed) {
+    try {
+        $sql = $db->prepare("INSERT INTO dogs VALUES (null, :name, :gender, :fixed)");
+        $sql->bindParam(':name', $name);
+        $sql->bindParam(':gender', $gender);
+        $sql->bindParam(':fixed', $fixed);
+        $sql->execute();
+        return $sql->rowCount();
+    } catch (PDOException $e) {
+        die("The was problem adding the dog.");
     }
 }
