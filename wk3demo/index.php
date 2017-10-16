c131396f1364267570130b4d83c3431ec6725e0c
Clark Alexander <calexander@neit.edu>
2017-10-16 09:22:14 -0400

    Get dogs as table

--- wk3demo/index.php
+++ wk3demo/index.php
@@ -1,5 +1,9 @@
 <?php
+require_once("assets/dbconn.php");
+require_once ("assets/dogs.php");
 include_once("assets/header.php");
+$db = dbConn();
+echo getDogsAsTable($db);
 include_once ("assets/footer.php");
 ?>
 
